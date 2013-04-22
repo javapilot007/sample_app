974248a9875ad7e1a19fba157196b059ea54710c
Java Pilot <javapilot007@hotmail.com>
2013-04-21 21:40:09 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a83ba8d2966bf44bec58bacc07aea14d2370e3027df773470c9847e124706d9ba08ac5013ee6132dcf5a10f2ab4033252bdc3c755fd9db56c8e981a4d304da7d'
