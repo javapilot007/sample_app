157987b7b2813f9aea0adf0db507cc459ba0b2d1
Java Pilot <javapilot007@hotmail.com>
2013-04-24 22:14:47 -0600

    RSpec enhancements

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,17 @@ describe "UserPages" do
   
   subject { page }
 
+  shared_examples_for "all user pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "Signup page" do
     before { visit signup_path } 
+    let(:heading)    { 'Sign up' }
+    let(:page_title) { 'Sign up' }
     
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) } 
+    it_should_behave_like "all user pages"
   end
 
 end
