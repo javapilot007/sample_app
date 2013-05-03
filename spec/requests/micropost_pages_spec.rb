59e0d6822d1b0542949656cbb0a1fdf69a9e4be3
Java Pilot <javapilot007@hotmail.com>
2013-05-02 18:51:05 -0600

    Micropost Deleting

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -17,16 +17,30 @@ describe "MicropostPages" do
 
       describe "error messages" do
         before { click_button "Post" }
+        
         it { should have_content('error') } 
       end
     end
 
     describe "with valid information" do
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
     end
-    
   end
+  
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
+  
 end
