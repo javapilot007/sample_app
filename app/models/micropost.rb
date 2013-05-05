e862566ac604ff12ff759f253396cd5637689d3c
Java Pilot <javapilot007@hotmail.com>
2013-05-04 18:26:57 -0600

    SQL Subsets on the Feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,8 +19,17 @@ class Micropost < ActiveRecord::Base
   
   default_scope order: 'microposts.created_at DESC'
   
+#  # Returns ALL microposts from the users being followed by the given user.
+#  def self.from_users_followed_by(user)
+#    followed_user_ids = user.followed_user_ids
+#    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+#  end
+  
+  # Returns a SQL subselect of all microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
   end  
 end
